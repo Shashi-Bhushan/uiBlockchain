b7103d625a352a15a1dda99d9f426621f86cd434
Shashi Bhushan <sbhushan1@outlook.com>
2017-08-06 10:10:24 +0530

    Initial Commit - v0.0.1

---
+++ index.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Shashi on 5/8/17.
+ */
+
+"use strict";
+
+const electron = require('electron');
+// app module controls your application lifecycle
+const app = electron.app;
+// Browser window module allows native window creation
+const browserWindow = electron.BrowserWindow;
+
+// Keep a global reference of the window object.
+// otherwise when JS object is grabage collected, window will close.
+let mainWindow;
+
+app.on('ready', onReady);
+
+function onReady() {
+    mainWindow = new browserWindow({
+        height: 600,
+        width : 600
+    });
+
+    mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+
+    // opens up the dev tools
+    //mainWindow.webContents.openDevTools();
+
+    mainWindow.on('closed', () => onClosed);
+}
+
+function onClosed() {
+    mainWindow = null;
+}
---
+++ test/index.js
@@ -0,0 +1,23 @@
+/**
+ * Created by shashi on 5/8/17.
+ */
+
+"use strict";
+
+const assert = require('assert');
+
+describe('UI Blockchain tests', function(){
+    it('sample test', function(){
+        assert.equal(1+1, 2);
+    });
+
+    it('sample test 2', function(){
+        assert.equal(1+1, 2);
+    });
+});
+
+describe('UI Blockchain tests 2', function(){
+    it('sample test', function(){
+        assert.equal(1+1, 2);
+    });
+});
